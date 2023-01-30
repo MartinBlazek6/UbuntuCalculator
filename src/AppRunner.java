f0adff965fa5141e9a3f548c47ccce06a413a624
Martin Blazek <martin.blazek@ness.com>
2023-01-30 14:52:00 +0100

    title added

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -2,6 +2,7 @@ public class AppRunner {
     public static void main(String[] args){
         Calculator calculator = new Calculator(450, 600,Helper.darkGrey);
         calculator.setLocationRelativeTo(null);
+        calculator.setTitle("Ubuntu calculator clone");
         calculator.setVisible(true);
     }
 }
